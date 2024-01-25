ed34168ad14895310e0fbd72183c9fc1f1637ef3
c-eric <43305167+c-eric@users.noreply.github.com>
2024-01-25 16:50:02 -0500

    Website v1
    Filled website with every pal by fetching it from API

---
+++ js/fetch-api.js
@@ -0,0 +1,61 @@
+const page = 1; // The page number to fetch
+const limit = 111; // The number of items per page
+
+fetch(`http://localhost:8080/?page=${page}&?limit=${limit}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // `data` is the response from your API
+    console.log(data);
+    console.log(data.content);
+    // console.log(data.content[0].name);
+
+    const infoDiv = document.getElementById("wrapper");
+    infoDiv.innerHTML = ""; // wipe demo cards
+
+    for (let i = 0; i < limit; i++) {
+        let elementHTML = "";
+
+        let palKey = data.content[i].key; // ID
+        let palIMG = data.content[i].imageWiki; // Image
+        let palName = data.content[i].name; // Name
+        let palElement1 = data.content[i].types[0] // Type 1
+        let palElement2 = data.content[i].types[1] // Type 2
+
+        // console.log(data.content[i].types[0]);
+
+        let palElementCap1 = capitalizeFirstLetter(palElement1) 
+
+        elementHTML = `<p class="element-text ${palElement1}-color">${palElementCap1}</p>`
+        
+        if (palElement2 != undefined) {
+            let palElementCap2 = capitalizeFirstLetter(palElement2)
+            elementHTML += `<p class="element-text ${palElement2}-color">${palElementCap2}</p>`
+        }
+        
+        const listItem = document.createElement("div");
+        listItem.className = "pal-info";
+        listItem.innerHTML = `
+        <p class="id">#${palKey}</p>
+        <img class="pal-img" src="${palIMG}" alt="">
+        <h5 class="pal-name">${palName}</h5>
+        <div class="element">
+            ${elementHTML}
+        </div>
+        `;
+        infoDiv.appendChild(listItem);
+    }
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+});
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+  
